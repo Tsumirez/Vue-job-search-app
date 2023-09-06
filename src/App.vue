bf48918d612cde22163a7d77d6b5169e033be983
zefirez <tsumirez@zoho.com>
2023-09-06 20:51:44 +0200

    Crated and added MainNav component
    View is kinda different here from react and angular.
    Not only do you have to import a child component,
    you also gotta declare it in component configuration object (that would be Vue 2 way if i'm correct)
    
    After that you can use it.
    
    Also the syntax of
    
    { property }
    
    is ES6 feature where just by naming the property it'll automatically
    set it's value to variable of same name as the property.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script setup></script>
 
 <template>
-  <h1 class="mt-8 text-center text-6xl">Zefirez Vue App</h1>
+  <MainNav />
 </template>
+
+<script>
+import MainNav from "@/components/MainNav.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainNav,
+  },
+};
+</script>
