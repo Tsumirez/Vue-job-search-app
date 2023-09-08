995910bb97b9433e2dd0338cb65379f63d38ac8f
zefirez <tsumirez@zoho.com>
2023-09-08 17:44:57 +0200

    added new css colors to tailwind config file
    By doing it that way, tailwind will generate a bunch of new css classes
    based on provided colors which we'll put to use later in the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans]
       }
     },
+    colors: {
+      "brand-gray-1": "#dadce0",
+      "brand-blue-1": "#19672d",
+      "brand-green-1": "#137333"
+    }
   },
   plugins: []
 }
