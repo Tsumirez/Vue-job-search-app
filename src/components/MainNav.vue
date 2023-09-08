3ec12165ec15c9beebbaac184366a93572447e59
zefirez <tsumirez@zoho.com>
2023-09-08 18:09:27 +0200

    added logo to MainNav component and some tailwind styling

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="w-full text-sm">Main Nav</header>
+  <header class="w-full text-sm">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
+      <div
+        class="border-brand-gray-1 flex h-full flex-nowrap border-b border-solid px-8"
+      >
+        <a href="/" class="flex h-full items-center text-xl">Zefirez Design</a>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
